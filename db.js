9f3f1485ead788671c199ec18d10f7bd0d80014d
Deniz Ozger <deniz@klarismo.com>
2016-02-13 18:41:54 +0000

    posting readings and dislaying readings table

--- db.js
+++ db.js
@@ -1,9 +1,27 @@
 const
   pg = require('pg');
 
-exports.test = (req, res) => {
+exports.addReading = (req, res) => {
   pg.connect(process.env.DATABASE_URL + '?ssl=true', function(err, client, done) {
-    client.query('SELECT * FROM test', function(err, result) {
+    client.query('INSERT INTO readings (id, type, value, created_at) VALUES($1, $2, $3, $4) RETURNING id',
+      [undefined, req.body.type, req.body.value, undefined],
+      function(err, result) {
+        done();
+        if (err) {
+          console.error(err);
+          res.send('Error ' + err);
+        }
+
+        console.log('Reading inserted with id: ' + result.rows[0].id);
+
+        res.sendStatus(200);
+    });
+  });
+};
+
+exports.getReadings = (req, res) => {
+  pg.connect(process.env.DATABASE_URL + '?ssl=true', function(err, client, done) {
+    client.query('SELECT * FROM readings', function(err, result) {
       done();
       if (err) {
         console.error(err);
--- index.js
+++ index.js
@@ -1,20 +1,31 @@
 // 'use strict';
 
 const
-  express = require('express'),
-  app     = express(),
-  db      = require('./db');
+  express     = require('express'),
+  app         = express(),
+  bodyParser  = require('body-parser'),
+  db          = require('./db');
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
+// views
 app.get('/', (req, res) => res.render('pages/index'));
-app.get('/db', db.test);
+
+// api
+app.post('/readings', db.addReading);
+app.get('/readings', db.getReadings);
 
 app.listen(app.get('port'), () => {
   console.log('Node app is running on port (◕‿◕)', app.get('port'));
+
+  if (!process.env.DATABASE_URL) {
+    console.error('DATABASE_URL not set!');
+  }
 });
 
 
