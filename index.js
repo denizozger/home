935034b51d87bfeeaebbfa6f31bbf81a0d6109a8
Deniz Ozger <deniz@klarismo.com>
2016-02-13 13:06:56 +0000

    arrow functions test

--- index.js
+++ index.js
@@ -1,20 +1,18 @@
-var express = require('express');
-var app = express();
+// 'use strict';
+
+const
+  express = require('express'),
+  app = express();
 
 app.set('port', (process.env.PORT || 5000));
-
 app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.get('/', function(request, response) {
-  response.render('pages/index');
-});
+app.get('/', (req, res) => res.render('pages/index'));
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+app.listen(app.get('port'), () => {
+  console.log('Node app is running on port (◕‿◕)', app.get('port'));
 });
 
 
