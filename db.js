33fc12187fbfc06b355d6694b8cd916bbb71570a
Deniz Ozger <denizozger@gmail.com>
2016-02-19 22:17:56 +0000

    save readings in the db

--- db.js
+++ db.js
@@ -1,40 +1,38 @@
+'use strict'
+
 const
   pg = require('pg');
 
-var inMemoryDb = [];
-
 exports.addReading = (req, res) => {
-  console.log('Reading received: %j', req.body)
-  inMemoryDb.push(req.body)
-  res.sendStatus(200);
-  // pg.connect(process.env.DATABASE_URL + '?ssl=true', function(err, client, done) {
-  //   client.query('INSERT INTO readings (id, type, value, created_at) VALUES($1, $2, $3, $4) RETURNING id',
-  //     [undefined, req.body.type, req.body.value, undefined],
-  //     function(err, result) {
-  //       done();
-  //       if (err) {
-  //         console.error(err);
-  //         res.send('Error ' + err);
-  //       }
+  pg.connect(process.env.DATABASE_URL + '?ssl=true', function(err, client, done) {
+    client.query('INSERT INTO readings (temperature, pressure, humidity, created_at) VALUES($1, $2, $3, $4) RETURNING id',
+      [req.body.temperature, req.body.pressure, req.body.humidity, req.body.time],
+      function(err, result) {
+        done();
+        if (err) {
+          console.error(err);
+          res.send('Error ' + err);
+        }
 
-  //       console.log('Reading inserted with id: ' + result.rows[0].id);
+        console.log('Reading [%j] inserted with id [%s] ', req.body, result.rows[0].id);
 
-  //       res.sendStatus(200);
-  //   });
-  // });
+        res.sendStatus(200);
+    });
+  });
 };
 
 exports.getReadings = (req, res) => {
-  res.send(inMemoryDb)
-  // pg.connect(process.env.DATABASE_URL + '?ssl=true', function(err, client, done) {
-  //   client.query('SELECT * FROM readings', function(err, result) {
-  //     done();
-  //     if (err) {
-  //       console.error(err);
-  //       res.send('Error ' + err);
-  //     }
+  pg.connect(process.env.DATABASE_URL + '?ssl=true', function(err, client, done) {
+    client.query('SELECT * FROM readings', function(err, result) {
+      done();
+      if (err) {
+        console.error(err);
+        res.send('Error ' + err);
+      }
 
-  //     res.render('pages/db', {results: result.rows} );
-  //   });
-  // });
+      let results = result.rows.sort((r1, r2) => r2.id - r1.id)
+
+      res.render('pages/db', { results: results } );
+    });
+  });
 };
